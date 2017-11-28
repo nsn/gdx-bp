ed443babdddd79077e2b655202449eaaa9ea185a
Michael Bayer <nsn@nightspawn.com>
2017-11-28 16:38:28 +0100

    GWT compatibility changes

--- src/main/java/com/nightspawn/gdxbp/GdxLogger.java
+++ src/main/java/com/nightspawn/gdxbp/GdxLogger.java
@@ -1,13 +1,11 @@
 package com.nightspawn.gdxbp;
 
-import java.util.IllegalFormatException;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.StringBuilder;
 
 /**
  * Simple logger, usage:
- * 
+ * <p>
  * <pre>
  *  ...
  *  private static final GdxLogger LOG = new GdxLogger(MyClass.class);
@@ -15,14 +13,13 @@ import com.badlogic.gdx.utils.StringBuilder;
  *  LOG.info("message one={}, two={}", obj1, obj3);
  *  ...
  * </pre>
- * 
- * @author nsn
  *
+ * @author nsn
  */
 public class GdxLogger {
     public static final char PLACEHOLDER_START = '{';
     public static final char PLACEHOLDER_END = '}';
-    private static final String ARG_MISSING = "[missing: %d]";
+    private static final String ARG_MISSING = "[missing: ";
     private final String tag;
 
     public GdxLogger(String tag) {
@@ -36,30 +33,28 @@ public class GdxLogger {
     }
 
     public String fmt(String format, Object... args) {
-        try {
-            if (args.length < 1) {
-                return format;
-            }
-            StringBuilder sb = new StringBuilder();
-            int end = format.indexOf(PLACEHOLDER_START), start = 0;
-            int index = 0;
-            do {
-                String before = format.substring(start, end);
-                sb.append(before);
-                if (args.length <= index) {
-                    sb.append(String.format(ARG_MISSING, index));
-                } else {
-                    sb.append(args[index]);
-                }
-                start = format.indexOf(PLACEHOLDER_END, end) + 1;
-                end = format.indexOf(PLACEHOLDER_START, start);
-                index++;
-            } while (end >= 0);
-            sb.append(format.substring(format.lastIndexOf(PLACEHOLDER_END) + 1));
-            return sb.toString();
-        } catch (IllegalFormatException e) {
-            return "Error formatting... " + e.getMessage();
+        if (args.length < 1) {
+            return format;
         }
+        StringBuilder sb = new StringBuilder();
+        int end = format.indexOf(PLACEHOLDER_START), start = 0;
+        int index = 0;
+        do {
+            String before = format.substring(start, end);
+            sb.append(before);
+            if (args.length <= index) {
+                sb.append(ARG_MISSING);
+                sb.append(index);
+                sb.append(']');
+            } else {
+                sb.append(args[index]);
+            }
+            start = format.indexOf(PLACEHOLDER_END, end) + 1;
+            end = format.indexOf(PLACEHOLDER_START, start);
+            index++;
+        } while (end >= 0);
+        sb.append(format.substring(format.lastIndexOf(PLACEHOLDER_END) + 1));
+        return sb.toString();
     }
 
     public void info(String msg, Object... args) {
